30097ba9aea3f76d9046cd9ec07dc4a59f4bdef2
sameerahex <sameer.rout@ahex.co.in>
2022-12-05 16:42:44 +0530

    distance

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,6 +10,7 @@ import * as jsonData from '../../assets/points.json';
 import { APIService } from '../api.service';
 import { HeaderServiceService } from '../module/header-service.service';
 import * as Notiflix from 'notiflix';
+import * as turf from '@turf/distance';
 
 @Component({
   selector: 'app-test',
@@ -20,50 +21,12 @@ export class TestComponent implements OnInit {
   map: any = {};
   title = 'gis-mapbox';
   style = 'mapbox://styles/mapbox/streets-v11';
-  geojson = {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        properties: {
-          message: 'Foo',
-          iconSize: [60, 60],
-        },
-        geometry: {
-          type: 'Point',
-          coordinates1: [-66.324462, -16.024695],
-        },
-      },
-      {
-        type: 'Feature',
-        properties: {
-          message: 'Bar',
-          iconSize: [50, 50],
-        },
-        geometry: {
-          type: 'Point',
-          coordinates1: [-61.21582, -15.971891],
-        },
-      },
-      {
-        type: 'Feature',
-        properties: {
-          message: 'Baz',
-          iconSize: [40, 40],
-        },
-        geometry: {
-          type: 'Point',
-          coordinates1: [-63.292236, -18.281518],
-        },
-      },
-    ],
-  };
 
   layerPaint = {
     'circle-radius': 10,
     'circle-color': '#3887be',
   };
-  coordinatesPoint: any[] = [-77.31373744962319, 45.22121694402196];
+  coordinatesPoint: any[] = [86.528876, 21.5152];
   points: any;
   // lat = -78.880453;
   // lang = 42.897852;
@@ -128,34 +91,53 @@ export class TestComponent implements OnInit {
     this.map.addControl(Draw);
 
     this.map.on('draw.create', (x: any) => {
-      console.log(x.features[0].geometry.coordinates);
+      console.log(x.features);
+      // updateArea() {
+      if (x.features[0].geometry.coordinates.length === 2) {
+        var from = (window as any).turf.point(
+          x.features[0].geometry.coordinates[0]
+        );
+        var to = (window as any).turf.point(
+          x.features[0].geometry.coordinates[1]
+        );
+        var options = { units: 'kilometers' };
 
-      this.drawPolygonalJson(
-        x.features[0].geometry.coordinates,
-        x.features[0].id
-      );
-      Notiflix.Confirm.prompt(
-        'Polygon',
-        'Enter Proper Name',
-        '',
-        'Ok',
-        'Cancel',
-        function okCb(clientAnswer) {
-          // array.push(x.features[0].geometry.coordinates);
-          Notiflix.Report.success(
-            clientAnswer,
-            x.features[0].geometry.coordinates.join(''),
-            'ok'
-          );
-          console.log(x.features[0].geometry.coordinates);
-        },
-        function cancelCb(clientAnswer) {
-          // console.log('Client answer was: ' + clientAnswer);
-        },
-        {
-          // Custom options
-        }
-      );
+        var distance = (window as any).turf.distance(from, to, options);
+        console.log(distance);
+        Notiflix.Report.info(
+          'Distance',
+          `Total distance from ${x.features[0].geometry.coordinates[0]} to ${x.features[0].geometry.coordinates[1]} is ${distance} Kilometer`,
+          'Ok'
+        );
+      } else {
+        this.drawPolygonalJson(
+          x.features[0].geometry.coordinates,
+          x.features[0].id
+        );
+        Notiflix.Confirm.prompt(
+          'Polygon',
+          'Enter Proper Name',
+          '',
+          'Ok',
+          'Cancel',
+          function okCb(clientAnswer) {
+            // array.push(x.features[0].geometry.coordinates);
+            Notiflix.Report.success(
+              clientAnswer,
+              x.features[0].geometry.coordinates.join(''),
+              'ok'
+            );
+
+            console.log(x.features[0].geometry.coordinates);
+          },
+          function cancelCb(clientAnswer) {
+            // console.log('Client answer was: ' + clientAnswer);
+          },
+          {
+            // Custom options
+          }
+        );
+      }
     });
   }
   drawPolygonalJson(cordinates: any, id: any) {
@@ -173,6 +155,113 @@ export class TestComponent implements OnInit {
           },
         },
         // data: jsonData,
+      });
+      this.map.addSource('places', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  '<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>',
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.038659, 38.931567],
+              },
+            },
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  '<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a Mad Men Season Five Finale Watch Party, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>',
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.003168, 38.894651],
+              },
+            },
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  '<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a Big Backyard Beach Bash and Wine Fest on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.</p>',
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.090372, 38.881189],
+              },
+            },
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  '<strong>Ballston Arts & Crafts Market</strong><p>The Ballston Arts & Crafts Market sets up shop next to the Ballston metro this Saturday for the first of five dates this summer. Nearly 35 artists and crafters will be on hand selling their wares. 10:00-4:00 p.m.</p>',
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.111561, 38.882342],
+              },
+            },
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  "<strong>Seersucker Bike Ride and Social</strong><p>Feeling dandy? Get fancy, grab your bike, and take part in this year's Seersucker Social bike ride from Dandies and Quaintrelles. After the ride enjoy a lawn party at Hillwood with jazz, cocktails, paper hat-making, and more. 11:00-7:00 p.m.</p>",
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.052477, 38.943951],
+              },
+            },
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  '<strong>Capital Pride Parade</strong><p>The annual Capital Pride Parade makes its way through Dupont this Saturday. 4:30 p.m. Free.</p>',
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.043444, 38.909664],
+              },
+            },
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  '<strong>Muhsinah</strong><p>Jazz-influenced hip hop artist Muhsinah plays the Black Cat (1811 14th Street NW) tonight with Exit Clov and Gods’illa. 9:00 p.m. $12.</p>',
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.031706, 38.914581],
+              },
+            },
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  "<strong>A Little Night Music</strong><p>The Arlington Players' production of Stephen Sondheim's <em>A Little Night Music</em> comes to the Kogod Cradle at The Mead Center for American Theater (1101 6th Street SW) this weekend and next. 8:00 p.m.</p>",
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.020945, 38.878241],
+              },
+            },
+            {
+              type: 'Feature',
+              properties: {
+                description:
+                  '<strong>Truckeroo</strong><p>Truckeroo brings dozens of food trucks, live music, and games to half and M Street SE (across from Navy Yard Metro Station) today from 11:00 a.m. to 11:00 p.m.</p>',
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [-77.007481, 38.876516],
+              },
+            },
+          ],
+        },
       });
       // ALL YOUR APPLICATION CODE
       // Add a new layer to visualize the polygon.
@@ -197,34 +286,6 @@ export class TestComponent implements OnInit {
           'line-width': 3,
         },
       });
-      for (let marker of this.geojson.features) {
-        // Create a DOM element for each marker.
-        const el = document.createElement('div');
-        const width = marker.properties.iconSize[0];
-        const height = marker.properties.iconSize[1];
-        el.className = 'marker';
-        el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
-        el.style.width = `${width}px`;
-        el.style.height = `${height}px`;
-        el.style.backgroundSize = '100%';
-        let markercord: any = marker.geometry.coordinates1;
-        new mapboxgl.Marker(el).setLngLat(markercord).addTo(this.map);
-        // el.addEventListener('click', () => {
-        //   window.alert(marker.properties.message);
-        // });
-        console.log(markercord);
-
-        // Add markers to the map.
-      }
-      // this.map.addLayer({
-      //   id: 'places',
-      //   type: 'symbol',
-      //   source: 'places',
-      //   layout: {
-      //     'icon-image': '{icon}',
-      //     'icon-allow-overlap': true,
-      //   },
-      // });
     });
   }
 }
